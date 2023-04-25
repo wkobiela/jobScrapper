e37e9b6f968129a261ee4ff5d426427666efea73
wiktor.kobiela <wuch.mail@gmail.com>
2023-04-25 18:49:17 +0200

    Update settings and readme

--- runner.py
+++ runner.py
@@ -3,22 +3,48 @@ from scrappers import nofluffjobs
 from scrappers import bulldogjob
 from modules import setup, common
 
-"""
-Parameters:
-- justjoinit.run(role(str), lvl(list), city(str)) -> always looks remote + city
-- nofluffjobs.run(...(site(str), role(str), lvl(str), city(str))) -> always looks remote + city
-- bulldogjob.run(...(ite(str), role(str), lvl(str), city(str)))
-"""
-
+# Excel settings
 EXCEL_NAME = 'jobs.xlsx'
 NOFLUFFJOBS_SHEET = 'NoFluffJobs'
 BULLDOGJOB_SHEET = 'BulldogJob'
 JUSTJOINIT_SHEET = 'JustJoinIt'
 
-NOFLUFFJOBS_URL = common.createLinks(site='NoFluffJobs', role="testing", lvl="junior,mid", city="Gdańsk")
-BULLDOGJOB_URL = common.createLinks(site='BulldogJob', role="qa,tester", lvl="junior,mid", city="Remote,Gdańsk")
+# Search params
+nofluffjobs_settings = {
+    "site": "NoFluffJobs",
+    "role": "testing",
+    "lvl": "junior,mid",
+    "city": "Gdańsk" 
+}
 
+bulldogjob_settings = {
+    "site": "BulldogJob",
+    "role": "qa,tester",
+    "lvl": "junior,medium",
+    "city": "Remote,Gdańsk" 
+}
+
+justjoinit_settings = {
+    "role": ["testing"],
+    "lvl": ["mid", "junior"],
+    "city": "Gdańsk" 
+}
+
+# Create links
+NOFLUFFJOBS_URL = common.createLinks(site=nofluffjobs_settings['site'], 
+                                    role=nofluffjobs_settings['role'], 
+                                    lvl=nofluffjobs_settings['lvl'], 
+                                    city=nofluffjobs_settings['city'])
+BULLDOGJOB_URL = common.createLinks(site=bulldogjob_settings['site'], 
+                                    role=bulldogjob_settings['role'], 
+                                    lvl=bulldogjob_settings['lvl'], 
+                                    city=bulldogjob_settings['city'])
+
+# Run setup and scrappers
 setup.run(EXCEL_NAME, NOFLUFFJOBS_SHEET, BULLDOGJOB_SHEET, JUSTJOINIT_SHEET)
 nofluffjobs.run(NOFLUFFJOBS_SHEET, NOFLUFFJOBS_URL)
 bulldogjob.run(BULLDOGJOB_SHEET, BULLDOGJOB_URL)
-justjoinit.run(JUSTJOINIT_SHEET, role=['testing'], lvl=["mid", "junior"], city='Gdańsk')
+justjoinit.run(JUSTJOINIT_SHEET, 
+                role=justjoinit_settings['role'], 
+                lvl=justjoinit_settings['lvl'], 
+                city=justjoinit_settings['city'])
