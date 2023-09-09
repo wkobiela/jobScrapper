81501e024513fa5c8b1c05b0924cd8d43593a995
wiktor.kobiela <wuch.mail@gmail.com>
2023-09-09 11:22:41 +0200

    Fix bulldogjob recheable test

--- tests/test_linksReachable.py
+++ tests/test_linksReachable.py
@@ -7,8 +7,12 @@ def test_is_NoFluffJobs_reachable():
     assert response.status_code == 200
     
 def test_is_BulldogJob_reachable():
-    link = createLinks(site='BulldogJob', role="qa", lvl="junior,mid", city="Remote,Gdańsk")
-    response = requests.get(link, timeout=120)
+    link = createLinks(site='BulldogJob', role="qa,tester", lvl="junior,medium", city="Remote,Gdańsk")
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+        Chrome/58.0.3029.110 Safari/537.36'
+    }
+    response = requests.get(link, timeout=120, headers=headers)
     assert response.status_code == 200
     
 def test_is_JustJoinIt_reachable():
