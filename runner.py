47b86f49b664d51c927d996727378054029b0b72
wkobiela <12174429+wkobiela@users.noreply.github.com>
2024-01-19 17:01:48 +0100

    [feature] Use config.json file (#55)
    * Try to use config.json file
    * Change test env
    * Back to default pipelines
    * Verify if file exists
    * Fix loading configuration
    * Update readme, remove deprecated
    * Fix indentations in config.json

--- runner.py
+++ runner.py
@@ -1,54 +1,59 @@
+import argparse
+import json
 from scrappers import justjoinit
 from scrappers import nofluffjobs
 from scrappers import bulldogjob
 from modules import setup, common, base_logger
 
-# Excel settings
-EXCEL_NAME = 'jobs.xlsx'
-NOFLUFFJOBS_SHEET = 'NoFluffJobs'
-BULLDOGJOB_SHEET = 'BulldogJob'
-JUSTJOINIT_SHEET = 'JustJoinIt'
 
-# Search params
-nofluffjobs_settings = {
-    "site": "NoFluffJobs",
-    "role": "testing",
-    "lvl": "junior,mid",
-    "city": "Gdańsk" 
-}
+def main(config_file):
+    try:
+        with open(config_file, 'r') as configuration:
+            config = json.load(configuration)
+    except FileNotFoundError as e:
+        base_logger.log.error(e)
+        exit(1)
 
-bulldogjob_settings = {
-    "site": "BulldogJob",
-    "role": "qa,tester",
-    "lvl": "junior,medium",
-    "city": "Remote,Gdańsk" 
-}
+    # Excel settings
+    EXCEL_NAME = config['excel_settings']['EXCEL_NAME']
+    NOFLUFFJOBS_SHEET = config['excel_settings']['NOFLUFFJOBS_SHEET']
+    BULLDOGJOB_SHEET = config['excel_settings']['BULLDOGJOB_SHEET']
+    JUSTJOINIT_SHEET = config['excel_settings']['JUSTJOINIT_SHEET']
 
-justjoinit_settings = {
-    "site": "JustjoinIt",
-    "role": "testing",
-    "lvl": "mid.senior",
-    "city": "Gdańsk" 
-}
+    # Search params
+    nofluffjobs_settings = config['search_params']['nofluffjobs_settings']
+    bulldogjob_settings = config['search_params']['bulldogjob_settings']
+    justjoinit_settings = config['search_params']['justjoinit_settings']
 
-base_logger.log.info("runner: Starting runner.")
-# Create links
-NOFLUFFJOBS_URL = common.createLinks(site=nofluffjobs_settings['site'], 
-                                    role=nofluffjobs_settings['role'], 
-                                    lvl=nofluffjobs_settings['lvl'], 
-                                    city=nofluffjobs_settings['city'])
-BULLDOGJOB_URL = common.createLinks(site=bulldogjob_settings['site'], 
-                                    role=bulldogjob_settings['role'], 
-                                    lvl=bulldogjob_settings['lvl'], 
-                                    city=bulldogjob_settings['city'])
-JUSTJOINIT_URL = common.createLinks(site=justjoinit_settings['site'],
-                                    role=justjoinit_settings['role'],
-                                    lvl=justjoinit_settings['lvl'],
-                                    city=justjoinit_settings['city'])
+    base_logger.log.info("runner: Starting runner.")
+    # Create links
+    NOFLUFFJOBS_URL = common.createLinks(site=nofluffjobs_settings['site'], 
+                                        role=nofluffjobs_settings['role'], 
+                                        lvl=nofluffjobs_settings['lvl'], 
+                                        city=nofluffjobs_settings['city'])
+    BULLDOGJOB_URL = common.createLinks(site=bulldogjob_settings['site'], 
+                                        role=bulldogjob_settings['role'], 
+                                        lvl=bulldogjob_settings['lvl'], 
+                                        city=bulldogjob_settings['city'])
+    JUSTJOINIT_URL = common.createLinks(site=justjoinit_settings['site'],
+                                        role=justjoinit_settings['role'],
+                                        lvl=justjoinit_settings['lvl'],
+                                        city=justjoinit_settings['city'])
 
-# Run setup and scrappers
-setup.run(EXCEL_NAME, NOFLUFFJOBS_SHEET, BULLDOGJOB_SHEET, JUSTJOINIT_SHEET)
-nofluffjobs.run(NOFLUFFJOBS_SHEET, NOFLUFFJOBS_URL)
-bulldogjob.run(BULLDOGJOB_SHEET, BULLDOGJOB_URL)
-justjoinit.run(JUSTJOINIT_SHEET, JUSTJOINIT_URL)
-base_logger.log.info("runner: Runner finished work.")
+    # Run setup and scrappers
+    setup.run(EXCEL_NAME, NOFLUFFJOBS_SHEET, BULLDOGJOB_SHEET, JUSTJOINIT_SHEET)
+    nofluffjobs.run(NOFLUFFJOBS_SHEET, NOFLUFFJOBS_URL)
+    bulldogjob.run(BULLDOGJOB_SHEET, BULLDOGJOB_URL)
+    justjoinit.run(JUSTJOINIT_SHEET, JUSTJOINIT_URL)
+    base_logger.log.info("runner: Runner finished work.")
+
+if __name__ == "__main__":
+    program_description = '''
+    jobScrapper -  Simplify your IT job search.
+    How to use: https://github.com/wkobiela/jobScrapper/blob/master/README.md'''
+    
+    parser = argparse.ArgumentParser(description=program_description, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("--config", help="Path to the configuration file", required=True)
+    args = parser.parse_args()
+    
+    main(args.config)
