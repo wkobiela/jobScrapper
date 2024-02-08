1725a35db22a0a97e71d8abcf992fe2c95855920
wkobiela <12174429+wkobiela@users.noreply.github.com>
2024-02-08 20:07:17 +0100

    [feature] Rebuild bulldogjob scrapper (#78)
    * Rebuild bulldogjob scrapper
    
    * Increase coverage
    
    * Fix pytest warnings of deprecated method
    
    * Fix test
    
    * Fix logging
    
    * Fix pylint
    
    * Some cov fixes
    
    * Remove unused import
    
    * Disable coverage for runner
    
    * Try pragma to disable coverage
    
    * Add  .coveragerc control file
    
    * Add coverage config option for pipeline

--- jobscrapper/scrappers/bulldogjob.py
+++ jobscrapper/scrappers/bulldogjob.py
@@ -9,65 +9,69 @@ log = logging.getLogger(__name__)
 class BulldogJob():
     def __init__(self):
         self.jobs_dict = {}
-        
-    def updateJobsDict(self, url):
-        max_pages = getPagesCount(url, 
-                    parent = 'li', 
-                    child = 'class', 
-                    regex='.*h-10 mx-1 rounded-full flex items-center.*')
-        text = ""
-        try:
-            for page_num in range(1, max_pages + 1):
-                headers = {
+        self.job_link_list = []
+        self.header = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
                     Chrome/58.0.3029.110 Safari/537.36'
                 }
-                page = requests.get(url+f"/page,{page_num}", timeout=120, headers=headers)
-                page_soup = BeautifulSoup(page.content, "html.parser")                
-                job_links_list = page_soup.find_all("a", {"href": re.compile('.*bulldogjob.pl/companies/jobs/.*')})
-                for job in job_links_list:
-                    try:
-                        #workarount for false job objects
-                        if not job.find('div', class_=re.compile("flex flex-col items-center relative my-auto", re.I)): 
-                            continue
+        
+    def getJobsLinkList(self, url):
+        max_pages = getPagesCount(url, parent = 'li', child = 'class', 
+            regex='.*h-10 mx-1 rounded-full flex items-center.*')
+        for page_number in range(1, max_pages + 1):
+            page = requests.get(url+f"/page,{page_number}", timeout=120, headers=self.header)
+            page_content = BeautifulSoup(page.content, "html.parser")
+            self.job_link_list += page_content.find_all("a", {"href": re.compile('.*bulldogjob.pl/companies/jobs/.*')})
+        return self.job_link_list
+        
+    def updateJobsDict(self, job_link_list):
+        text = ""
+        try:
+            for job in job_link_list:
+                try:
+                    #workarount for false job objects
+                    if not job.find('div', class_=re.compile("flex flex-col items-center relative my-auto", re.I)): 
+                        continue
 
-                        job_link = job.get('href')
+                    job_link = job.get('href')
+                    job_title = job.find(name="h3", class_=re.compile("md:mb-5 lg:mb-0 md:text-18 text", re.I))
+                    if job_title is not None:
+                        job_title = job_title.find(string=True, recursive=False).text
+                    else:
+                        job_title="Regex error."
+                    
+                    job_company = job.find('div', class_=re.compile("text-xxs uppercase", re.I))
+                    if job_company is not None:
+                        job_company = job_company.find(string=True, recursive=True) is not None
+                    else:
+                        job_company = "Regex error"
+                    
+                    job_salary = job.find('div', class_=re.compile("lg:font-extrabold md:text-xl text-dm", re.I))
+                    if job_salary is not None:
+                        job_salary = job_salary.find(string=True, recursive=True).text
+                    else:
+                        job_salary="No information or regex error"
                         
-                        job_title = job.find(name="h3", class_=re.compile("md:mb-5 lg:mb-0 md:text-18 text", re.I))
-                        if job_title.find(text=True, recursive=True) is not None:
-                            job_title = job_title.find(text=True, recursive=False).text
-                        else:
-                            job_title="Regex error."
-                        
-                        job_company = job.find('div', class_=re.compile("text-xxs uppercase", re.I))
-                        if job_company.find(text=True, recursive=True) is not None:
-                            job_company = job_company.find(text=True, recursive=True) is not None
-                        else:
-                            job_company = "Regex error"
-                        
-                        job_salary = job.find('div', class_=re.compile("lg:font-extrabold md:text-xl text-dm", re.I))
-                        if job_salary is not None:
-                            job_salary = job_salary.find(text=True, recursive=True).text
-                        else:
-                            job_salary="No information or regex error"
-                            
-                        job_overall_info = job.find_all('div', class_=re.compile("flex items-start", re.I))
-                        for info in job_overall_info:
-                            text = text + info.find('span').text + " / "
+                    job_overall_info = job.find_all('div', class_=re.compile("flex items-start", re.I))
+                    for info in job_overall_info:
+                        text = text + info.find('span').text + " / "
 
-                        self.jobs_dict[job_link] = {"Title": [job_title], 
-                                                    "Company": [job_company], 
-                                                    "Salary": [job_salary], 
-                                                    "Location": [text]}
-                        text = ""
-                    except Exception as ie:
-                        log.error('bulldogjob:updateJobsDict: Exception %s on %s.', ie, job) 
+                    self.jobs_dict[job_link] = {"Title": [job_title], 
+                                                "Company": [job_company], 
+                                                "Salary": [job_salary], 
+                                                "Location": [text]}
+                    text = ""
+                except Exception as ie:
+                    log.error('bulldogjob:updateJobsDict: Exception %s on %s.', ie, job) 
         except Exception as e:
-            log.error('bulldogjob:updateJobsDict: Exception %s.', e)                
+            log.error('bulldogjob:updateJobsDict: Exception %s.', e)
+            raise     
+        return self.jobs_dict           
             
-def run(sheet, url):  
+def run(sheet, url): # pragma: no cover
     log.info("bulldogjob:run: Starting BulldogJob scrapper.")
     bull = BulldogJob()
-    bull.updateJobsDict(url)
-    updateExcel(sheet, bull.jobs_dict)
+    jobs_list = bull.getJobsLinkList(url)
+    jobs_dict = bull.updateJobsDict(jobs_list)
+    updateExcel(sheet, jobs_dict)
     log.info("bulldogjob:run: Finished BulldogJob scrapper.")
---
+++ tests/test_bulldogjob.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import pytest
+from bs4 import BeautifulSoup
+from jobscrapper.scrappers.bulldogjob import BulldogJob
+
+this_directory = Path(__file__).parent
+url = 'https://bulldogjob.pl/companies/jobs/s/role,qa,tester/experienceLevel,junior,medium/city,Remote,Gdansk'
+
+@pytest.fixture
+def _bulldogjob():
+    bulldog = BulldogJob()
+    return bulldog
+
+def test_creating_link_list(_bulldogjob):
+    jobs_link_list = _bulldogjob.getJobsLinkList(url)
+    assert len(jobs_link_list) != 0
+
+def test_create_jobs_info_dict(_bulldogjob): 
+    job_link_list = []
+    with open(this_directory / 'testfiles/bulldog_example_jobs_link_list', encoding="utf8") as f:
+        for line in f:
+            job_link_list += BeautifulSoup(line, 'html.parser')
+    jobs_info_dict = _bulldogjob.updateJobsDict(job_link_list)
+    assert len(jobs_info_dict) == 11
+    
+def test_create_jobs_info_dict_wrong_list(_bulldogjob):
+    wrong_job_link_list = 5
+    with pytest.raises(TypeError):
+        _bulldogjob.updateJobsDict(wrong_job_link_list)
+    
+    
