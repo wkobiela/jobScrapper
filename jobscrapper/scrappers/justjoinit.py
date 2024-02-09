d7dbae411978d557d49e8d78704afdc168fe1be1
wkobiela <wuch.mail@gmail.com>
2024-02-09 12:50:10 +0100

    Add tests

--- jobscrapper/scrappers/justjoinit.py
+++ jobscrapper/scrappers/justjoinit.py
@@ -17,31 +17,35 @@ class JustJoinIt():
         page = requests.get(url, timeout=120)
         page_soup = BeautifulSoup(page.content, "html.parser")
         self.job_links_list = page_soup.find_all("div", {"class": "css-1iq2gw3"}) 
-        return self.job_links_list
+        return self.job_links_list, self.domainName
 
-    def updateJobsDict(self, job_links_list):
+    def updateJobsDict(self, job_links_list, domainName):
         try:
             for job in job_links_list:
-                job_link = "https://"+self.domainName+job.find('a',  class_='css-4lqp8g')['href']
-                job_title = job.find('h2').text
-                job_company = job.find('div', class_=re.compile("css-aryx9u", re.I)).text 
-                job_salary = job.find('div', class_=re.compile("css-17pspck", re.I)).text
-                job_location = job.find('div', class_=re.compile("css-11qgze1", re.I)).text
-                if any in (job_link, job_title, job_company, job_salary, job_location) is None:
-                    log.error('justjoinit:updateJobsDict: Regex need to be fixed, not working for job: %s', job)
-                    break
-                self.jobs_dict[job_link] = {"Title": [job_title], 
-                                            "Company": [job_company], 
-                                            "Salary": [job_salary], 
-                                            "Location": [job_location]}
+                try:
+                    job_link = "https://"+domainName+job.find('a',  class_='css-4lqp8g')['href']
+                    job_title = job.find('h2').text
+                    job_company = job.find('div', class_=re.compile("css-aryx9u", re.I)).text 
+                    job_salary = job.find('div', class_=re.compile("css-17pspck", re.I)).text
+                    job_location = job.find('div', class_=re.compile("css-11qgze1", re.I)).text
+                    if any in (job_link, job_title, job_company, job_salary, job_location) is None:
+                        log.error('justjoinit:updateJobsDict: Regex need to be fixed, not working for job: %s', job)
+                        break
+                    self.jobs_dict[job_link] = {"Title": [job_title], 
+                                                "Company": [job_company], 
+                                                "Salary": [job_salary], 
+                                                "Location": [job_location]}
+                except Exception as ie:
+                    log.error('justjoinit:updateJobsDict: Exception %s on %s.', ie, job) 
         except Exception as e:
             log.error('justjoinit:updateJobsDict: Exception %s.', e)
+            raise
         return self.jobs_dict
 
 def run(sheetname, url): # pragma: no cover
     log.info("justjoinit:run: Starting JustJointIt scrapper.")
     just = JustJoinIt()
-    jobs_list = just.getJobsLinkList(url)
-    jobs_dict = just.updateJobsDict(jobs_list)
+    jobs_list, domain = just.getJobsLinkList(url)
+    jobs_dict = just.updateJobsDict(jobs_list, domain)
     updateExcel(sheetname, jobs_dict)
     log.info("justjoinit:run: Finished JustJoinIt scrapper.")
---
+++ tests/test_justjoinit.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import pytest
+from bs4 import BeautifulSoup
+from jobscrapper.scrappers.justjoinit import JustJoinIt
+
+this_directory = Path(__file__).parent
+url = 'https://justjoin.it/gdansk/testing/experience-level_mid.senior/remote_yes'
+
+@pytest.fixture
+def _justjoinit():
+    justjoinit = JustJoinIt()
+    return justjoinit
+
+def test_creating_link_list(_justjoinit):
+    jobs_link_list, _ = _justjoinit.getJobsLinkList(url)
+    assert len(jobs_link_list) != 0
+    
+def test_getting_domain(_justjoinit):
+    _, domain = _justjoinit.getJobsLinkList(url)
+    assert domain == 'justjoin.it'
+
+def test_create_jobs_info_dict(_justjoinit): 
+    job_link_list = []
+    with open(this_directory / 'testfiles/justjoinit_example_jobs_link_list', encoding="utf8") as f:
+        for line in f:
+            job_link_list += BeautifulSoup(line, 'html.parser')    
+    jobs_dict = _justjoinit.updateJobsDict(job_link_list, 'justjoin.it')
+    assert len(jobs_dict) == 38
+    
+def test_create_jobs_info_dict_wrong_list(_justjoinit):
+    wrong_job_link_list = 5
+    with pytest.raises(TypeError):
+        _justjoinit.updateJobsDict(wrong_job_link_list, 'justjoin.it')
+    
+    
