8b6f443103cb654eb895bf1a04b2fd7435cf99c5
wkobiela <wuch.mail@gmail.com>
2024-02-09 17:51:20 +0100

    Fix coverage

--- jobscrapper/scrappers/justjoinit.py
+++ jobscrapper/scrappers/justjoinit.py
@@ -24,13 +24,27 @@ class JustJoinIt():
             for job in job_links_list:
                 try:
                     job_link = "https://"+domainName+job.find('a',  class_='css-4lqp8g')['href']
-                    job_title = job.find('h2').text
-                    job_company = job.find('div', class_=re.compile("css-aryx9u", re.I)).text 
-                    job_salary = job.find('div', class_=re.compile("css-17pspck", re.I)).text
-                    job_location = job.find('div', class_=re.compile("css-11qgze1", re.I)).text
-                    if any in (job_link, job_title, job_company, job_salary, job_location) is None:
-                        log.error('justjoinit:updateJobsDict: Regex need to be fixed, not working for job: %s', job)
-                        break
+                    job_title = job.find('h2')
+                    if job_title is not None:
+                        job_title = job_title.find(string=True, recursive=False).text
+                    else:
+                        job_title = "Regex error."
+                    job_company = job.find('div', class_=re.compile("css-aryx9u", re.I))
+                    if job_company is not None:
+                        job_company = job_company.find(string=True, recursive=True).text
+                    else:
+                        job_company = "Regex error"
+                    job_salary = job.find('div', class_=re.compile("css-17pspck", re.I))
+                    if job_salary is not None:
+                        job_salary = job_salary.find(string=True, recursive=True).text
+                    else: 
+                        job_salary = "Regex error"
+                    job_location = job.find('div', class_=re.compile("css-11qgze1", re.I))
+                    if job_location is not None:
+                        job_location = job_location.find(string=True, recursive=True).text
+                    else: 
+                        job_location = "Regex error"
+
                     self.jobs_dict[job_link] = {"Title": [job_title], 
                                                 "Company": [job_company], 
                                                 "Salary": [job_salary], 
